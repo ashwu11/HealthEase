3f8954b61407188dffca140bb5f0517a80954c41
tracy-l-a <tracy.la829@gmail.com>
2024-03-09 21:31:51 -0800

    x icon

---
+++ src/components/x-icon.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+function XIcon() {
+    return (
+
+        <div
+            className="flex flex-col items-center px-3 pt-3 pb-2 rounded-2xl shadow-sm bg-blue-950 h-[54px] w-[54px] m-4">
+            <img
+
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/e1244aaec4f7ef0ca6ea614bcdd7e4ddb052a11e8973be0f1f1d4ec1e737f35e?"
+                className="w-full aspect-square"
+            />
+        </div>
+    );
+}
+
+export default XIcon;
