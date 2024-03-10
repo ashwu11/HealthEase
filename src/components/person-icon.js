12db9de65fb47dd2094a298fdb467f38f06be0b9
tracy-l-a <tracy.la829@gmail.com>
2024-03-09 19:51:45 -0800

    added tailwind css file

---
+++ src/components/person-icon.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+function PersonIcon() {
+    return (
+        <div
+            className="flex justify-center items-center px-5 rounded-2xl shadow-sm bg-blue-500 h-[136px] w-[136px]">
+            <img
+                loading="lazy"
+                alt="filler text change this hahahahahahaah"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/7289337d4b00c879912cde7429cf49a480daea5515cf8b008abbc6e64d201775?apiKey=f6bdfdfe9ef944de9a9a5f2bf6573abd&"
+                className="w-full aspect-square"
+            />
+
+
+        </div>
+
+    );
+}
+
+export default PersonIcon;
--- src/pages/camera-page.js
+++ src/pages/camera-page.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ImageGrid from '../components/logo';
+import PersonIcon from '../components/person-icon';
 
 function CameraPage() {
     return (
         <div>
             <h1>My App</h1>
             <ImageGrid />
+
+            <h2>trial icon</h2>
+            <PersonIcon />
         </div>
     );
 };
