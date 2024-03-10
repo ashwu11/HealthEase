7ab7eb5787ba529e4275ac99f52c370adefedc7c
Ashley Wu <ashleywu2004@gmail.com>
2024-03-10 08:18:10 -0700

    update search page and styling

--- src/pages/search-page.js
+++ src/pages/search-page.js
@@ -7,15 +7,19 @@ function SearchPage() {
   const navigate = useNavigate();
   const [inputValue, setInputValue] = useState("How can we help?");
   var [serverResponse, setServerResponse] = useState(null); // State variable to store the server response
+  const [isLoading, setIsLoading] = useState(false);
 
   const sendDataToServer = async () => {
     try {
+        setIsLoading(true);
+        const message = inputValue + " Summarize it in 3 sentences.";
+
       const response = await fetch('http://localhost:8000/send-data', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ data: inputValue }),
+        body: JSON.stringify({ data: message}),
       });
 
       console.log(response.text);
@@ -30,6 +34,8 @@ function SearchPage() {
       }
     } catch (error) {
       console.error('Error sending data to server:', error);
+    } finally {
+        setIsLoading(false);
     }
   };
   
@@ -38,23 +44,25 @@ function SearchPage() {
   }
 
   return (
-    <div className='Page'>
+    <div className='SearchPage'>
       <div className='search-page-top'>
         <div onClick={() => navigate('/home')}><XIcon /></div>
-        <h1>Search Page</h1>
-        <PersonIcon />
+        <h2 style={{ textAlign: 'left', fontSize: '8vw', marginLeft: '8vw', marginRight: '10vw'}}>Search Page</h2>
       </div>
 
       <div>
         <input className="search-bar" onChange={handleChange} value={inputValue}></input>
-        <button onClick={sendDataToServer}>Enter</button>
+        <button className="search-enter" onClick={sendDataToServer}>Enter</button>
       </div>
+
+      {/* Display the loading indicator */}
+      {isLoading && <p className='search-response'>Loading...</p>}
 
       {/* Display the server response as text */}
       {serverResponse && (
-        <div>
-            <h2>Server Response:</h2>
-            <p>{serverResponse}</p>
+        <div className='search-response'>
+            <h2 style={{fontSize: '6vw'}}>Answer: </h2>
+            <p style={{fontSize: '3vw'}}>{serverResponse}</p>
         </div>
         )}
 
