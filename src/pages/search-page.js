fc0edb00e9915235b1e778620120115943df4d79
Ashley Wu <ashleywu2004@gmail.com>
2024-03-10 00:49:31 -0800

    work on search page

--- src/pages/search-page.js
+++ src/pages/search-page.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SearchPage() {
 
+    let [input, setInput] = useState("How can we help?");
+    
+    function handleClick() {
+        alert(input);
+
+    }
+
+    function handleChange(e) {
+        setInput(e.target.value);
+    }
+
   return (
-    <div>
+    <div className='search-page'>
         <h1>Search Page</h1>
+        <input onChange={handleChange} value={input}></input>
+        <button onClick={handleClick}>Enter</button>
     </div>
   );
 };
