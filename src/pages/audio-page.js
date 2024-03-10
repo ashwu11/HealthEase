7dfeae251dec0f0895bc24d91540b52debd1d3ac
Ashley Wu <ashleywu2004@gmail.com>
2024-03-10 03:36:47 -0700

    modify server and search page

--- src/pages/audio-page.js
+++ src/pages/audio-page.js
@@ -5,6 +5,7 @@ function AudioPage() {
   return (
     <div>
         <h1>Audio Page</h1>
+        <p>Current input: </p>
     </div>
   );
 };
--- src/pages/search-page.js
+++ src/pages/search-page.js
@@ -1,23 +1,50 @@
 import React, { useState } from 'react';
+import XIcon from '../components/x-icon';
+import PersonIcon from '../components/person-icon';
+import { useNavigate } from 'react-router-dom';
 
 function SearchPage() {
 
-    let [input, setInput] = useState("How can we help?");
+    const navigate = useNavigate();
+    const [inputValue, setInputValue] = useState("How can we help?");
+
+    const sendDataToServer = async () => {
+        try {
+          const response = await fetch('http://localhost:8000/send-data', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ data: inputValue }),
+          });
     
-    function handleClick() {
-        alert(input);
-
-    }
-
+          if (response.ok) {
+            console.log('Data sent successfully');
+          } else {
+            console.error('Failed to send data to server');
+          }
+        } catch (error) {
+          console.error('Error sending data to server:', error);
+        }
+      };
+    
     function handleChange(e) {
-        setInput(e.target.value);
+        setInputValue(e.target.value);
     }
 
   return (
-    <div className='search-page'>
-        <h1>Search Page</h1>
-        <input onChange={handleChange} value={input}></input>
-        <button onClick={handleClick}>Enter</button>
+    <div className='Page'>
+        <div className='search-page-top'>
+            <div onClick={() => navigate('/home')}><XIcon/></div>
+            <h1>Search Page</h1>
+            <PersonIcon />
+        </div>
+        
+        <div>
+            <input className="search-bar" onChange={handleChange} value={inputValue}></input>
+            <button onClick={sendDataToServer}>Enter</button>
+        </div>
+        
     </div>
   );
 };
